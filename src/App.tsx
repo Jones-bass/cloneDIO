68b8cc60f74c52b51395b0dee635173d4c7f5d65
jones-bass <jonesbass.tb@gmail.com>
2022-10-31 23:41:35 -0300

    Estilizando Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 
-import Home from './pages/Home'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+import { GlobalStyle } from './styles/global';
 
 function App() {
-
   return (
-    <div>
-      <Home />
-    </div>)
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes >
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,19 @@
+
+
+import {InputContainer, InputText, IconContainer } from './styles';
+
+interface InputProps {
+  name: string
+  leftIcon?: any;
+}
+
+export default function Input ({leftIcon, name, ...rest}: InputProps) {
+  return (
+    <InputContainer>
+        {leftIcon ? (<IconContainer>{leftIcon}</IconContainer>) : null}
+        <InputText {...rest} />       
+    </InputContainer>
+  )
+}
+
+
