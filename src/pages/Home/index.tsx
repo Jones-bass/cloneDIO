8c36b02aa1f10aa1c3adab82f6ac3731d0c6f9e3
jones-bass <jonesbass.tb@gmail.com>
2022-10-31 22:16:36 -0300

    Estilizando Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,31 @@
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Container, Title, TitleHighlight, TextContent } from '../../styles/home';
+import bannerImage from '../../assets/banner.png'
+
 
 export default function Home() {
-
   return (
-    <div>
-      <Header/>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <div>
+          <Title>
+            <TitleHighlight>
+              Implemente <br />
+            </TitleHighlight>
+            o seu futuro global agora!</Title>
+          <TextContent>Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo
+            desafio profissional, evoluindo em comunidade com os melhores experts.</TextContent>
+          <Button title="Começar agora" variant="secondary" onClick={() => (console.log('Teste'))} />
+        </div>
+        <div>
+          <img src={bannerImage} alt="Imagem principal do site." />
+        </div>
+      </Container>
+    </>
   )
 }
+
+
 
